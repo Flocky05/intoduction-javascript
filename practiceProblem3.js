748c4baf324a17d6a6eede34375a80ed7a56c778
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-08-01 09:55:43 +0000

    added practice Problem -3 solution

---
+++ practiceProblem3.js
@@ -0,0 +1,19 @@
+var A=12;
+var B=22;
+var C=34;
+if(A>B && A>C){
+    console.log('largest number is A');
+} else if(B>A && B>C){
+    console.log('largest number is B');
+} else{
+    console.log('largest number is C');
+}
+
+var X=9;
+var Y=10;
+var Z=11;
+if((X==Y) || (X==Z) || (Y==Z)){
+    console.log('triangle is Isosceles');
+}else{
+    console.log('triangle is not Isosceles');
+}
